29e958316f17c495c4d1eb6a19fc434911fec0e8
Wesley Levasseur <mailpro.levasseur@icloud.com>
2023-07-07 17:49:31 +0200

    Update Header

--- src/libs/components/Header.svelte
+++ src/libs/components/Header.svelte
@@ -1,10 +1,17 @@
 <script lang='ts'>
 	import { onMount } from 'svelte';
+	import { checkAuth } from '../functions/auth-functions';
+	import { page } from '$app/stores';
 
 	export let actualPathName;
 
-	onMount(() => {
+	let loading = true;
+	let logged = false;
+
+	onMount(async () => {
 		actualPathName = window.location.pathname;
+		logged = await checkAuth($page.url) === true;
+		loading = false;
 	});
 </script>
 
@@ -18,8 +25,24 @@
 			</a>
 			<nav>
 				<a class={actualPathName === '/' ? 'actual' : ''} href='/'>Accueil</a>
-				<a class={actualPathName === '/auth/sign-up' ? 'actual' : ''} href='/auth/sign-up'>Inscription</a>
-				<a class={actualPathName === '/auth/sign-in' ? 'actual' : ''} href='/auth/sign-in'>Connexion</a>
+				{#if loading}
+					<a class='spinner' href={undefined}>
+						<p>•</p>
+					</a>
+				{:else}
+					{#if logged}
+						<a class={actualPathName === '/u/task' ? 'actual' : ''} href='/u/task'>Tâches</a>
+						<a class={actualPathName === '/u/profile' ? 'actual' : ''} href='/u/profile'>
+							<i class='fa fa-user'></i>
+						</a>
+						<a class={actualPathName === '/u/logout' ? 'actual' : ''} href='/u/logout'>
+							<i class='fa-solid fa-arrow-right-from-bracket'></i>
+						</a>
+					{:else}
+						<a class={actualPathName === '/auth/sign-up' ? 'actual' : ''} href='/auth/sign-up'>Inscription</a>
+						<a class={actualPathName === '/auth/sign-in' ? 'actual' : ''} href='/auth/sign-in'>Connexion</a>
+					{/if}
+				{/if}
 			</nav>
 		</article>
 	</section>
