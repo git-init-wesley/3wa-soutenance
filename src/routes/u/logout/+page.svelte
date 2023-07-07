53cc5ab41c4771f370c8300662d961813f4b7c83
Wesley Levasseur <mailpro.levasseur@icloud.com>
2023-07-07 17:49:38 +0200

    Add /u/logout

---
+++ src/routes/u/logout/+page.svelte
@@ -0,0 +1,12 @@
+<script lang='ts'>
+	import Preloader from '../../../libs/components/Preloader.svelte';
+	import { goto } from '$app/navigation';
+
+	localStorage.removeItem('auth_token');
+	localStorage.removeItem('auth_mail');
+
+	goto('/').then();
+</script>
+<!-- ========================= Preloader Start ========================= -->
+<Preloader></Preloader>
+<!-- ========================= Preloader End ========================= -->
